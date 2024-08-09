8465790fedab6c18a3ba8084ee2e157feaf0ce9e
Benjamin <benjaminngyujin@gmail.com>
2024-08-09 17:26:20 +0800

    init commit

---
+++ src/auth_client_demo_assets/svelte/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import { onNavigate } from '$app/navigation'
+
+    import '../app.css'
+	onNavigate((navigation) => {
+		// @ts-ignore
+		if (!document.startViewTransition) return
+
+		return new Promise((resolve) => {
+			// @ts-ignore
+			document.startViewTransition(async () => {
+				resolve()
+				await navigation.complete
+			})
+		})
+	})
+</script>
+
+<div class="hidden sm:block">
+	<!-- <Header /> -->
+</div>
+<main class='titanium'>
+    <slot />
+</main>
